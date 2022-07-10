61c743c1cd59aeaf38a4cefe0ae0329a98770087
rayct <rayct@icloud.com>
2022-07-10 02:12:51 +0100

    Started Auth, Login and Signup Screens

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-// import 'package:tiktok_clone/controllers/auth_controller.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:tiktok_clone/controllers/auth_controller.dart';
 // import 'package:tiktok_clone/views/screens/add_video_screen.dart';
 // import 'package:tiktok_clone/views/screens/profile_screen.dart';
 // import 'package:tiktok_clone/views/screens/search_screen.dart';
@@ -21,10 +21,10 @@ const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
 const borderColor = Colors.grey;
 
-// // FIREBASE
-// var firebaseAuth = FirebaseAuth.instance;
-// var firebaseStorage = FirebaseStorage.instance;
-// var firestore = FirebaseFirestore.instance;
+// FIREBASE
+var firebaseAuth = FirebaseAuth.instance;
+var firebaseStorage = FirebaseStorage.instance;
+var firestore = FirebaseFirestore.instance;
 
-// // CONTROLLER
-// var authController = AuthController.instance;
+// CONTROLLER
+var authController = AuthController.instance;
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
 import 'package:tiktok_clone/constants.dart';
-// import 'package:tiktok_clone/controllers/auth_controller.dart';
-// import 'package:tiktok_clone/views/screens/auth/login_screen.dart';
-// ignore: unused_import
-// import 'package:tiktok_clone/views/screens/auth/signup_screen.dart';
+import 'package:tiktok_clone/controllers/auth_controller.dart';
+import 'package:tiktok_clone/views/screens/auth/login_screen.dart';
+import 'package:tiktok_clone/views/screens/auth/signup_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp().then((value) {
-  //   Get.put(AuthController());
-  // });
+  await Firebase.initializeApp().then((value) {
+    Get.put(AuthController());
+  });
   runApp(const MyApp());
 }
 
@@ -20,13 +19,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'TikTok Clone',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const Text('Hello Ray'),
+      home: SignupScreen(),
     );
   }
 }
