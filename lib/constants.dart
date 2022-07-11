744432ced56b00642b9f73ef5b0c3a25af9be131
rayct <rayct@icloud.com>
2022-07-11 01:16:46 +0100

    Comments and Video Screens

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,18 +3,18 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:tiktok_clone/controllers/auth_controller.dart';
-// import 'package:tiktok_clone/views/screens/add_video_screen.dart';
-// import 'package:tiktok_clone/views/screens/profile_screen.dart';
-// import 'package:tiktok_clone/views/screens/search_screen.dart';
-// import 'package:tiktok_clone/views/screens/video_screen.dart';
+import 'package:tiktok_clone/views/screens/add_video_screen.dart';
+import 'package:tiktok_clone/views/screens/profile_screen.dart';
+import 'package:tiktok_clone/views/screens/search_screen.dart';
+import 'package:tiktok_clone/views/screens/video_screen.dart';
 
-// List pages = [
-//   VideoScreen(),
-//   SearchScreen(),
-//   const AddVideoScreen(),
-//   Text('Messages Screen'),
-//   ProfileScreen(uid: authController.user.uid),
-// ];
+List pages = [
+  VideoScreen(),
+  SearchScreen(),
+  const AddVideoScreen(),
+  Text('Messages Screen'),
+  ProfileScreen(uid: authController.user.uid),
+];
 
 const pages = [
   Text('Home Screen'),
